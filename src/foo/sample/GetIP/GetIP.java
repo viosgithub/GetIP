66002642b4caa914d039dfdd07f18c3833e4a885
vios@ylab <mori@.(none)>
2011-07-01 15:07:18 +0900

    netstatを使うようにしたが大した情報が表示されない．やはり/proc/<PIP>/net/uipを使うべきか，ただそれだとパーミッション関係でうまくいかない

--- src/foo/sample/GetIP/GetIP.java
+++ src/foo/sample/GetIP/GetIP.java
@@ -1,6 +1,8 @@
 package foo.sample.GetIP;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,6 +15,8 @@ public class GetIP extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
 	Process proc = null;
 	EditText etNum = null;
+	BufferedReader br = null;
+    String outStr = "";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,7 @@ public class GetIP extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
+		/*
 		int i = Integer.valueOf(etNum.getText().toString());
 				Log.d("debug","try to get proccess" + i);
             try {
@@ -35,6 +40,36 @@ public class GetIP extends Activity implements OnClickListener {
 			if(proc != null)
 			{
 				Log.d("debug",proc.getOutputStream().toString());
+			}
+			*/
+            try {
+				 proc = Runtime.getRuntime().exec(
+				     "netstat");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(proc != null)
+			{
+               br = new BufferedReader(new InputStreamReader(proc.getInputStream()), 4056);
+               try{
+               while(true)
+               {
+					outStr = br.readLine();
+            	  if (outStr != null && outStr.length() != 0)
+            		  {
+            		  	Log.d("debug",outStr);
+            		  }
+            	  else
+            	  {
+            		  break;
+            	  }
+               }
+               }
+               catch(IOException e)
+               {
+            	  e.printStackTrace(); 
+               }
 			}
         	
 	}
